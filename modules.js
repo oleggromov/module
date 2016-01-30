799f0e5f2f87bbbc0f65c837818e5eccb5a49506
Gromych <gromych@yandex-team.ru>
2016-01-30 18:32:00 +0300

    partially working version

--- modules.js
+++ modules.js
@@ -1,12 +1,63 @@
 (function (global, undefined) {
+    "use strict";
+
+    /**
+     * Module import function. Use it to declare
+     * dependencies (as the first argument) and module body (the second one).
+     *
+     * @param {Array} dependencies Array of dependencies (paths) to load.
+     * @param {Function} callback Module body.
+     */
+    global.imp = function (dependencies, callback) {
+        _imports.push({
+            callback: callback,
+            dependencies: dependencies
+        });
+
+        console.log(dependencies, _imports);
+
+        each(dependencies, loadOnce);
+    };
+
+    /**
+     * Module export function.
+     *
+     * @param  {[type]} name     [description]
+     * @param  {[type]} exported [description]
+     * @return {[type]}          [description]
+     */
+    global.exp = function (name, exported) {
+        _state[name] = _LOADED;
+        _modules[name] = exported;
+    };
+
+
+    /**
+     * Private part.
+     */
+
     var _modules = {};
-    var _loading = {};
+    var _state = {};
     var _imports = [];
+
+    var _UNKNOWN = undefined;
+    var _WAITING = 1;
+    var _LOADED = 2;
 
     function each (arr, callback) {
         for (var i = 0; i < arr.length; i++) {
             callback(arr[i], i);
         }
+    }
+
+    function flatten (arr) {
+        var flat = [];
+        each(arr, function (item) {
+            if (item) {
+                flat.push(item);
+            }
+        });
+        return flat;
     }
 
     function load (src, callback) {
@@ -21,9 +72,11 @@
     }
 
     function loadOnce (dependency) {
-        if (!_loading[dependency]) {
-            _loading[dependency] = true;
+        if (_state[dependency] === _UNKNOWN) {
+            _state[dependency] = _WAITING;
             load(dependency, checkImports);
+        } else if (_state[dependency] === _LOADED) {
+            checkImports();
         }
     }
 
@@ -35,33 +88,26 @@
         return params;
     }
 
-    function checkImports (src) {
+    function checkImports () {
         each(_imports, function (imp, index) {
+            if (!imp) {
+                return;
+            }
+
             var fulfilled = true;
 
             each(imp.dependencies, function (dep) {
-                if (!_modules[dep]) {
+                if (_state[dep] !== _LOADED) {
                     fulfilled = false;
                 }
             });
 
             if (fulfilled) {
                 imp.callback.apply(undefined, getArguments(imp.dependencies));
-                _imports.splice(index, 1);
+                _imports[index] = undefined;
             }
         });
+
+        _imports = flatten(_imports);
     }
-
-    global.imp = function (dependencies, callback) {
-        _imports.push({
-            callback: callback,
-            dependencies: dependencies
-        });
-
-        each(dependencies, loadOnce);
-    };
-
-    global.exp = function (name, exported) {
-        _modules[name] = exported;
-    };
 })(window);
